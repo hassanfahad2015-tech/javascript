cf4cf768d2275a7ebc112a744ab7b5e7fb8df645
Hassan hussein <hassanfahad2015@gmail.com>
2026-03-08 16:45:21 +0200

    x x C mu

---
+++ variable_and_data_types/variables.js
@@ -0,0 +1,13 @@
+
+
+var firstName = "Ali" 
+var age = 23
+var lastName = "Hussein"
+console.log(firstName);
+console.log(lastName);
+
+console.log (typeof firstName)
+console.log(typeof age)
+
+var IslightOnly = false
+console.log(typeof IslightOnly)
